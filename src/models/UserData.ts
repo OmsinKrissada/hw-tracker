0c6ba37da578c0f66adc9014427814ebd33fd978
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2021-06-14 00:52:50 +0700

    changes dev script (due to vulnerability in ts-node-dev)

---
+++ src/models/UserData.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column, PrimaryColumn, ManyToOne } from "typeorm";
+
+@Entity({ synchronize: true })
+export class UserData {
+
+	@PrimaryColumn({ type: "varchar", length: 18 })
+	id: string;
+
+	// @ManyToOne()
+	@Column()
+	homework_id: number
+
+
+
+
+}
