cf205c6b1fc7fd4846666ccbeaa8c4f3fec54ac7
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2022-07-31 13:06:44 +0700

    fixed docs base path

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
 	title: 'HW Tracker',
 	description: 'API docs for curious developers',
-	base: '/docs/',
+	base: '/api/docs/',
 	themeConfig: {
 		logo: 'https://images-ext-2.discordapp.net/external/0t7xHbvlC6ezNEJ_kwHbt4VDUd-ga6GZLDW5av7QgfU/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/848986970108723280/09c59fb5dbb5ad3f32769d2488339bcc.png?width=671&height=671',
 		nav: [
